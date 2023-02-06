18882c419571d7ebbc143abbe8a7ccd5379c2d4e
Federico <isma-fb@hotmail.com>
2023-02-06 19:06:42 +0100

    hidden en botones (corregir)

--- routes/news.js
+++ routes/news.js
@@ -5,28 +5,25 @@ const connection = dbConnection()
 
 router.get('/', (req, res, next) => {     
     let page = req.query.page
+    let pagina = Number(req.params.id)
     let cantNews = 5
     let inicio = (cantNews * page) - cantNews 
     let siguiente = Number(page)+1
     let atras = Number(page)-1
-    if (atras >= 0){
+    
+    if (atras <= 0){
         atras = 1
     }
-    console.log(page)
-    console.log(siguiente)    
-
     
     connection.query(`SELECT * FROM news LIMIT ${inicio}, ${cantNews}`, (error, result) => {
         // console.log(error)
-        // console.log(result)
-
+        console.log(result)
         res.render('news/news.ejs', {
             news: result,
             page,
             siguiente, 
-            atras
-            
-
+            atras,   
+       
         })
     })
 });
@@ -34,6 +31,8 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
     console.log(req.body)
     const {title, news} = req.body
+
+    
 
     connection.query("INSERT INTO news SET ?",{
         title,
